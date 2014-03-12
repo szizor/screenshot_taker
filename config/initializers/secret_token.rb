6fa02c19bc1cee46e9f41ad9ec6fcb34a5836357
Alejandro <alejandro@freshout.us>
2014-03-11 18:27:00 -0600

    add config ignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScreenShots::Application.config.secret_token = '40d0d8725e04bd2c14264c326292640e2700a1c3e65a7dbb4356c2941ad9bc72d7ac620982e51e278d5e35c19fc8d9cb563ef05b254c94bb821895274065196a'
