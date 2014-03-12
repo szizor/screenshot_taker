b2ae6dd2dca5601a24490a38413cd068ad3cc427
Alejandro <alejandro@freshout.us>
2014-03-11 18:18:21 -0600

    change default

--- db/migrate/20130626141400_create_sitemaps_table.rb
+++ db/migrate/20130626141400_create_sitemaps_table.rb
@@ -2,7 +2,7 @@ class CreateSitemapsTable < ActiveRecord::Migration
   def self.up
     create_table :sitemaps do |t|
       t.string :url
-      t.boolean :processed, :default => 0
+      t.boolean :processed, :default => false
       t.timestamps
     end
   end
